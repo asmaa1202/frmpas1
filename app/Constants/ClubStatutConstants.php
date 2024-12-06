aa26dc7118bd8cf2d3a2d2de5134273f2afad829
imane-pro <imanearroub67@gmail.com>
2024-12-06 11:58:43 +0100

    statistique modif

---
+++ app/Constants/ClubStatutConstants.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+
+namespace App\Constants;
+
+class ClubStatutConstants
+{
+    const STATUT_EN_COURS = 'en cours';
+    const STATUT_ACCEPTER = 'accepter';
+    const STATUT_REFUSER = 'refuser';
+}
+
